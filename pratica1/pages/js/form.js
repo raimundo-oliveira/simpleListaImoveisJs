027bb36557dcec01e38d81676869f1520211a22f
airton-silva <airtonsilvan89@gmail.com>
2021-06-17 08:53:14 -0300

    Correção de duplicação da lista

--- pratica1/pages/js/form.js
+++ pratica1/pages/js/form.js
@@ -8,9 +8,9 @@ function Mudarestado(el) {
 }
 
 function adicionar() {
-    var form = document.getElementById('form');
+
     var titulo = document.getElementById('titulo');
-    var descricao = document.getElementById('descriao');
+    var descricao = document.getElementById('descricao');
     var quartos = document.getElementById('quartos');
     var vagaG = document.getElementById('vagaG');
     var areaT = document.getElementById('areaT');
@@ -19,34 +19,31 @@ function adicionar() {
     var preco = document.getElementById('preco');
     var image = document.getElementById('image');
 
-    form.addEventListener('submit', function(e) {
         // alerta o valor do campo
-        var lista = document.getElementById('lista').innerHTML;
-        var img = document.getElementById('imagem').innerHTML;
-        img.src = image;
-        lista += "<li>"+"Titulo: " +titulo.value+ "<br>"+
+    let lista = document.getElementById('lista').innerHTML;
+    let im = document.getElementById('imagem').src = "'"+ image +"'";
+    
+          lista += "<li>"+"Titulo: " +titulo.value+ "<br>"+
                         "Descrição" +descricao.value+ "<br>"+
                         "Quartos" +quartos.value+ "<br>"+
                         "Vaga D Garagem" +vagaG.value+ "<br>"+
                         "Área do Terreno" +areaT.value+ "<br>"+
                         "Área Contruída" +areaC.value+ "<br>"+
                         "Localidade" +local.value+ "<br>"+
-                        "Preço" +preco.value+ "<br>"+ 
+                        "Preço" +preco.value+ "<br>"+ "<img src='' id= 'imagem'>"+
                  "</li>" + "<hr>"   
         
-        document.getElementById('lista').innerHTML = lista +img;
+        document.getElementById('lista').innerHTML = lista;
+
+        limparCampus();
+
     
-        // impede o envio do form
-        e.preventDefault();
-    
-    });
-    limparCampus();
     Mudarestado("minhaDiv");
 }
 
 function limparCampus() {
     var titulo = document.getElementById('titulo').value = null;
-    var descricao = document.getElementById('descriao').value = null;
+    var descricao = document.getElementById('descricao').value = null;
     var quartos = document.getElementById('quartos').value = null;
     var vagaG = document.getElementById('vagaG').value = null;
     var areaT = document.getElementById('areaT').value = null;
