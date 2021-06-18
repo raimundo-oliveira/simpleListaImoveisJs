e0c22f0448581e655b3f10ed08eeea8e8cf1f18d
airton-silva <airtonsilvan89@gmail.com>
2021-06-18 14:05:30 -0300

    função imovel e correção de bug da imagem

--- pratica1/pages/js/form.js
+++ pratica1/pages/js/form.js
@@ -6,39 +6,68 @@ function Mudarestado(el) {
         document.getElementById(el).style.display = 'none';
 
 }
+//titulo, descricao, quartos, vagaG, areaT, areaCst,local, preco, path
+function imoveis(titulo, descricao, quartos, vagaG, areaT, areaCst,local, preco, path){
+    const imovelList = 
+        {
+            titulo: titulo, 
+            descricao: descricao, 
+            quartos: quartos, 
+            vagaG: vagaG, 
+            areaT: areaT, 
+            areaCst: areaCst,
+            local: local, 
+            preco: preco, 
+            path: path
+        };
+    
+    //alert(imovelList);
+    return imovelList;
+
+
+
+}
 
 function adicionar() {
 
-    var titulo = document.getElementById('titulo');
-    var descricao = document.getElementById('descricao');
-    var quartos = document.getElementById('quartos');
-    var vagaG = document.getElementById('vagaG');
-    var areaT = document.getElementById('areaT');
-    var areaC = document.getElementById('areaC');
-    var local = document.getElementById('localidade');
-    var preco = document.getElementById('preco');
-    var image = document.getElementById('image');
+    var titulo = document.getElementById('titulo').value;
+    var descricao = document.getElementById('descricao').value;
+    var quartos = document.getElementById('quartos').value;
+    var vagaG = document.getElementById('vagaG').value;
+    var areaT = document.getElementById('areaT').value;
+    var areaC = document.getElementById('areaC').value;
+    var local = document.getElementById('localidade').value;
+    var preco = document.getElementById('preco').value;
+    var path = document.getElementById('image').value;
 
-        // alerta o valor do campo
-    let lista = document.getElementById('lista').innerHTML;
-    
+    let list = [];
+    list = new imoveis(titulo, descricao, quartos, vagaG, areaT, areaC,local, preco, path);
 
-          lista += "<li>"+"Titulo: " +titulo.value+ "<br>"+
-                        "Descrição" +descricao.value+ "<br>"+
-                        "Quartos" +quartos.value+ "<br>"+
-                        "Vaga D Garagem" +vagaG.value+ "<br>"+
-                        "Área do Terreno" +areaT.value+ "<br>"+
-                        "Área Contruída" +areaC.value+ "<br>"+
-                        "Localidade" +local.value+ "<br>"+
-                        "Preço" +preco.value+ "<br>"+ "<img src='' id= 'imagem'>"+
-                 "</li>" + "<hr>"   
-        
-        document.getElementById('lista').innerHTML = lista;
+    limparCampus();
 
-        limparCampus();
-
-    
     Mudarestado("minhaDiv");
+
+   show(list);
+}
+
+function show(lista) {
+    
+    let l = document.getElementById('lista').innerHTML;
+
+    l += "<li>"+"Titulo: " +lista.titulo+ "<br>"+
+                  "Descrição: " +lista.descricao+ "<br>"+
+                  "Quartos: " +lista.quartos+ "<br>"+
+                  "Vaga D Garagem: " +lista.vagaG+ "<br>"+
+                  "Área do Terreno: " +lista.areaT+ "<br>"+
+                  "Área Contruída: " +lista.areaC+ "<br>"+
+                  "Localidade: " +lista.local+ "<br>"+
+                  "Preço: " +lista.preco+ "<br>"+"<br>"+ 
+                  "<img src='"+lista.path+"' id='imagem'>"+
+           "</li>" + "<hr>"   
+  
+  document.getElementById('lista').innerHTML = l;
+  
+  //document.getElementById("imagem").src = lista.path;
 }
 
 function limparCampus() {
@@ -50,5 +79,5 @@ function limparCampus() {
     var areaC = document.getElementById('areaC').value = null;
     var local = document.getElementById('localidade').value = null;
     var preco = document.getElementById('preco').value = null;
-    var image = document.getElementById('image').value = null;
+    var path = document.getElementById('image').value = null;
 }
